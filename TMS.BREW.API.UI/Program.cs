716438d4de963e46f2cfa5466426f6ad0fea03d1
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2020-11-25 22:59:52 +0300

    feat: add menu items

--- TMS.BREW.API.UI/Program.cs
+++ TMS.BREW.API.UI/Program.cs
@@ -13,63 +13,72 @@ namespace TMS.BREW.API.UI
     {
         static void Main(string[] args)
         {
-            ShowUserMenu();
             RequestService reqService = new RequestService();
             Parameters commons = new Parameters();
-            switch (Console.ReadKey().Key)
+            while (true)
             {
-                case ConsoleKey.N:
-                    Console.Write("Enter Name : ");
-                    var nameBrewery =
-                        reqService.RequestByName(Console.ReadLine()).Result;
-                    if (nameBrewery.Count() != 0)
-                    {
-                        MoveToBinFile(nameBrewery, commons.filePathName);
-                        PrintList(nameBrewery);
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("No items found.");
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    break;
-                case ConsoleKey.C:
-                    Console.Write("Enter city : ");
-                    var cityBrewery = reqService.RequestDataByCity(Console.ReadLine()).Result;
-                    if (cityBrewery.Count() != 0)
-                    {
-                        MoveToBinFile(cityBrewery, commons.filePathCity);
-                        PrintList(cityBrewery);
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("No items found.");
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    break;
-                case ConsoleKey.T:
-                    var typeBrewery = reqService.RequestByType().Result;
-                    if (typeBrewery.Count() != 0)
-                    {
-                        MoveToBinFile(typeBrewery, commons.filePathType);
-                        PrintList(typeBrewery);
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("No items found.");
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    break;
-                case ConsoleKey.F:
-                    var fullList = reqService.RequestFullList().Result;
-                    MoveToBinFile(fullList, commons.filePathFull);
-                    PrintList(fullList);
-                    break;
+                Console.WriteLine();
+                ShowUserMenu();
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.N:
+                        Console.WriteLine("+++++++");
+                        Console.Write("Enter Name : ");
+                        var nameBrewery =
+                            reqService.RequestByName(Console.ReadLine()).Result;
+                        if (nameBrewery.Count() != 0)
+                        {
+                            MoveToBinFile(nameBrewery, commons.filePathName);
+                            PrintList(nameBrewery);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("No items found.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case ConsoleKey.C:
+                        Console.WriteLine("+++++++");
+                        Console.Write("Enter city : ");
+                        var cityBrewery = reqService.RequestDataByCity(Console.ReadLine()).Result;
+                        if (cityBrewery.Count() != 0)
+                        {
+                            MoveToBinFile(cityBrewery, commons.filePathCity);
+                            PrintList(cityBrewery);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("No items found.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case ConsoleKey.T:
+                        Console.WriteLine("+++++++");
+                        var typeBrewery = reqService.RequestByType().Result;
+                        if (typeBrewery.Count() != 0)
+                        {
+                            MoveToBinFile(typeBrewery, commons.filePathType);
+                            PrintList(typeBrewery);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("No items found.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    case ConsoleKey.F:
+                        var fullList = reqService.RequestFullList().Result;
+                        MoveToBinFile(fullList, commons.filePathFull);
+                        PrintList(fullList);
+                        break;
+                    case ConsoleKey.E:
+                        Environment.Exit(0);
+                        break;
+                }
             }
-                Console.ReadKey();
         }
         public static void ShowUserMenu()
         {
@@ -81,6 +90,8 @@ namespace TMS.BREW.API.UI
             Console.WriteLine("Search by City - \"C\"");
             Console.WriteLine("Search by Type - \"T\"");
             Console.WriteLine("Show full list - \"F\"");
+            Console.WriteLine("Exit - \"E\"");
+            Console.Write("Enter : ");
         }
         public static void MoveToBinFile(IEnumerable<Brewery> brewList, string filePath)
         {
